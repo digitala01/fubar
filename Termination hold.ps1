c88414fbbb188cbaf02e94d3de6a687065b9c404
digitala01 <digitala01@gmail.com>
2017-06-28 10:28:53 -0500

    Update Termination hold.ps1

--- Termination hold.ps1
+++ Termination hold.ps1
@@ -1,2 +1,2 @@
 ﻿$Name = Read-Host -Prompt 'Enter Domain Username'
-New-MailboxSearch "$Name - Termination" -SourceMailboxes "$Name@whitestonereit.com" -InPlaceHoldEnabled $true
+New-MailboxSearch "$Name - Termination" -SourceMailboxes "$name@domain" -InPlaceHoldEnabled $true
